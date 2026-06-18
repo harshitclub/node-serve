c53e11c87f1b3895046fad43d15f330fe3677883
Harshit Kumar <harshitclub@gmail.com>
2026-06-18 16:40:53 +0530

    feat: dockerize this app

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import http from "http";
-import fs from "fs";
 
-const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -23,6 +21,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
